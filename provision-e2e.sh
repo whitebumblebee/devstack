454d82de7fcdda086dda4598f03dd03e9b3361d3
UsamaSadiq <usama.sadiq@arbisoft.com>
2022-07-19 11:00:35 +0500

    feat!: Rename studio service to cms

--- provision-e2e.sh
+++ provision-e2e.sh
@@ -10,15 +10,15 @@ elif [ ! -d "$DEVSTACK_WORKSPACE" ]; then
     exit 1
 fi
 
-# Copy the test course tarball into the studio container
-docker cp "${DEVSTACK_WORKSPACE}/edx-e2e-tests/upload_files/course.tar.gz" "$(make --silent --no-print-directory dev.print-container.studio)":/tmp/
+# Copy the test course tarball into the cms container
+docker cp "${DEVSTACK_WORKSPACE}/edx-e2e-tests/upload_files/course.tar.gz" "$(make --silent --no-print-directory dev.print-container.cms)":/tmp/
 
 # Extract the test course tarball
-docker-compose exec -T studio bash -e -c 'cd /tmp && tar xzf course.tar.gz'
+docker-compose exec -T cms bash -e -c 'cd /tmp && tar xzf course.tar.gz'
 
 # Import the course content
-docker-compose exec -T studio bash -e -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py cms --settings=devstack_docker import /tmp course'
+docker-compose exec -T cms bash -e -c 'source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py cms --settings=devstack_docker import /tmp course'
 
 # Clean up the temp files
-docker-compose exec -T studio bash -e -c 'rm /tmp/course.tar.gz'
-docker-compose exec -T studio bash -e -c 'rm -r /tmp/course'
+docker-compose exec -T cms bash -e -c 'rm /tmp/course.tar.gz'
+docker-compose exec -T cms bash -e -c 'rm -r /tmp/course'
